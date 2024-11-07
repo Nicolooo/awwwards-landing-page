7ae8da24db52b72615668cb9b92461131b595168
Nicolo <71701653+Nicolooo@users.noreply.github.com>
2024-11-07 21:30:29 +0100

    updd

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -29,7 +29,8 @@ export default function Contact() {  // Renamed component to "Contact"
                                 src="/images/pfp.jpg"
                             />
                         </div>
-                        <h2>Let's work</h2>
+                        <h2>Let&apos;s work</h2>
+
                     </span>
                     <h2>together</h2>
                     <motion.div style={{ x }} className={styles.buttonContainer}>
